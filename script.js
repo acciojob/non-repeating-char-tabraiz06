913c0cc852dbd70fb84e41164e7bde080a33e8bd
Md Tabraiz <138916881+tabraiz06@users.noreply.github.com>
2024-11-30 19:19:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstNonRepeatedChar(str) {
+	
  // Write your code here
+		let arr= str.trim().split("")
+	let result=""
+	for(let i=0; i<arr.length; i++){
+	   arr.filter(ele=> ele === arr[i]).length >1 ? result=null : result=arr[i]
+	}
+	console.log(result)
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
