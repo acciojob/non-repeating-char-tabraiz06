910fd64d35f526b54023ab41342c04f09d2aff71
Md Tabraiz <138916881+tabraiz06@users.noreply.github.com>
2024-12-02 18:27:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 function firstNonRepeatedChar(str) {
-	
- // Write your code here
-		let arr= str.trim().split("")
-	let result = [];
+  // Write your code here
+  let arr = str.trim().split("");
+  let res = {};
   for (let i = 0; i < arr.length; i++) {
-    arr.filter((ele) => ele === arr[i]).length > 1
-      ? result.push(null)
-      : result.push(arr[i]);
+    if (res[arr[i]]) {
+      res[arr[i]] = res[arr[i]] + 1;
+    } else {
+      res[arr[i]] = 1;
+    }
   }
-  return result.find((ele) => ele !== null)
-    ? result.find((ele) => ele !== null)
-    : null;
+  let result=Object.values(res).sort((a,b)=>a-b)
+  
+  if(result.includes(1)){
+ return  Object.keys(res).find(ele=>res[ele] === result[0])
+  }else{
+    return null
+  }
+
 }
 const input = prompt("Enter a string");
 
