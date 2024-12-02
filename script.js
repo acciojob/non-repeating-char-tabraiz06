aa94874ef9f2cbaf1736153e2e8a7fcecb145e7d
Md Tabraiz <138916881+tabraiz06@users.noreply.github.com>
2024-12-02 18:01:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ function firstNonRepeatedChar(str) {
 	
  // Write your code here
 		let arr= str.trim().split("")
-	let result=""
-	for(let i=0; i<arr.length; i++){
-	   arr.filter(ele=> ele === arr[i]).length >1 ? result=null : result=arr[i]
-	}
-	console.log(result)
+	let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    arr.filter((ele) => ele === arr[i]).length > 1
+      ? result.push(null)
+      : result.push(arr[i]);
+  }
+  return result.find((ele) => ele !== null)
+    ? result.find((ele) => ele !== null)
+    : null;
 }
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+
+let res = console.log(firstNonRepeatedChar(input));
+alert(console.log(firstNonRepeatedChar(input))); 
